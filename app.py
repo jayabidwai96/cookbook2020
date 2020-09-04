5dd4f4cc19566128b01b87931b8f2cbf19c25887
jayabidwai96 <jayabidwai96@gmail.com>
2020-09-04 04:21:19 +0000

    initail commit

---
+++ app.py
@@ -0,0 +1,23 @@
+  
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_tasks():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
